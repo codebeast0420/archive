f6b9b44c296934e28f674a4f5da413aaccd10bf8
John <codebeast0420@gmail.com>
2023-11-22 08:08:24 +0100

    basic crop function

---
+++ Object Remover/View/ImageCropperView.swift
@@ -0,0 +1,70 @@
+//
+//  ImageCropper.swift
+//  Avatarly
+//
+//  Created by Shahid Iqbal on 22/08/2023.
+//
+
+import SwiftUI
+import Mantis
+
+struct ImageCropperView: UIViewControllerRepresentable {
+    @Binding var isShowing : Bool
+    @Binding var image: UIImage?
+    @State var cropShapeType: Mantis.CropShapeType
+    @State var presetFixedRatioType: Mantis.PresetFixedRatioType
+    @Binding var ratio: Double
+    @Environment(\.presentationMode) var presentationMode
+
+    class Coordinator: CropViewControllerDelegate {
+        func cropViewControllerDidImageTransformed(_ cropViewController: CropViewController) {
+
+        }
+
+        var parent: ImageCropperView
+
+        init(_ parent: ImageCropperView) {
+            self.parent = parent
+        }
+
+        func cropViewControllerDidCrop(_ cropViewController: CropViewController, cropped: UIImage, transformation: Transformation, cropInfo: CropInfo) {
+            parent.image = cropped
+            print("transformation is \(transformation)")
+            parent.isShowing=false
+//            parent.gotoImageEdit = true
+//            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func cropViewControllerDidCancel(_ cropViewController: CropViewController, original: UIImage) {
+            parent.isShowing=false
+//            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func cropViewControllerDidFailToCrop(_ cropViewController: CropViewController, original: UIImage) {
+        }
+
+        func cropViewControllerDidBeginResize(_ cropViewController: CropViewController) {
+        }
+
+        func cropViewControllerDidEndResize(_ cropViewController: CropViewController, original: UIImage, cropInfo: CropInfo) {
+        }
+    }
+
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> CropViewController {
+//        var config = Mantis.Config()
+        
+        let cropViewController = Mantis.cropViewController(image: image!)
+        cropViewController.delegate = context.coordinator
+//        cropViewController.didSelectRatio(ratio: 5.0);
+        return cropViewController
+    }
+
+    func updateUIViewController(_ uiViewController: CropViewController, context: Context) {
+        uiViewController.didSelectRatio(ratio: ratio)
+    }
+}
